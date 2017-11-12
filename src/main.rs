710836cb34861603370db22450ef4f355ab90b57
Klaus Purer <klaus.purer@gmail.com>
2017-11-12 18:57:37 +0100

    feat(twitter): Integrate Twitter API with egg-mode

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,64 @@
+extern crate egg_mode;
 extern crate mammut;
+#[macro_use]
+extern crate serde_derive;
+extern crate tokio_core;
 extern crate toml;
+
 use mammut::{Data, Mastodon, Registration};
 use mammut::apps::{AppBuilder, Scope};
 use std::io;
 use std::fs::File;
 use std::io::prelude::*;
+use tokio_core::reactor::Core;
 
 fn main() {
-    let mastodon = match File::open("mastodon-twitter-sync.toml") {
-        Ok(f) => load_from_config(f),
-        Err(_) => register(),
+    let mastodon = match File::open("mastodon.toml") {
+        Ok(f) => mastodon_load_from_config(f),
+        Err(_) => mastodon_register(),
     };
 
-    let account = mastodon.verify();
-    println!("{:?}", account);
+    let account = mastodon.verify().unwrap();
+    let mastodon_statuses = mastodon.statuses(account.id, false, true).unwrap();
+
+    let twitter_config = match File::open("twitter.toml") {
+        Ok(f) => twitter_load_from_config(f),
+        Err(_) => twitter_register(),
+    };
+
+    let con_token =
+        egg_mode::KeyPair::new(twitter_config.consumer_key, twitter_config.consumer_secret);
+    let access_token = egg_mode::KeyPair::new(
+        twitter_config.access_token,
+        twitter_config.access_token_secret,
+    );
+    let token = egg_mode::Token::Access {
+        consumer: con_token,
+        access: access_token,
+    };
+
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+    /*let result = core.run(egg_mode::tweet::user_timeline(
+        twitter_config.user_id,
+        false,
+        true,
+        &token,
+        &handle,
+    )).unwrap();*/
+    let mut timeline =
+        egg_mode::tweet::user_timeline(twitter_config.user_id, false, true, &token, &handle)
+            .with_page_size(50);
+    for tweet in &core.run(timeline.start()).unwrap() {
+        println!(
+            "<@{}> {}",
+            tweet.user.as_ref().unwrap().screen_name,
+            tweet.text
+        );
+    }
 }
 
-fn register() -> Mastodon {
+fn mastodon_register() -> Mastodon {
     let app = AppBuilder {
         client_name: "mastodon-twitter-sync",
         redirect_uris: "urn:ietf:wg:oauth:2.0:oob",
@@ -28,23 +70,95 @@ fn register() -> Mastodon {
     registration.register(app).unwrap();;
     let url = registration.authorise().unwrap();
     println!("Click this link to authorize on Mastodon: {}", url);
-    println!("Paste the returned authorization code: ");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
 
-    let code = input.trim();
+    let code = console_input("Paste the returned authorization code");
     let mastodon = registration.create_access_token(code.to_string()).unwrap();
 
     // Save app data for using on the next run.
     let toml = toml::to_string(&*mastodon).unwrap();
-    let mut file = File::create("mastodon-twitter-sync.toml").unwrap();
+    let mut file = File::create("mastodon.toml").unwrap();
     file.write_all(toml.as_bytes()).unwrap();
     mastodon
 }
 
-fn load_from_config(mut file: File) -> Mastodon {
+fn mastodon_load_from_config(mut file: File) -> Mastodon {
     let mut config = String::new();
     file.read_to_string(&mut config).unwrap();
     let data: Data = toml::from_str(&config).unwrap();
     Mastodon::from_data(data)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TwitterConfig {
+    consumer_key: String,
+    consumer_secret: String,
+    access_token: String,
+    access_token_secret: String,
+    user_id: u64,
+    user_name: String,
+}
+
+fn twitter_register() -> TwitterConfig {
+    println!("Go to https://apps.twitter.com/app/new to create a new Twitter app.");
+    println!("Name: Mastodon Twitter Sync");
+    println!("Description: Synchronizes Tweets and Toots");
+    println!("Website: https://github.com/klausi/mastodon-twitter-sync");
+
+    let consumer_key = console_input("Paste your consumer key");
+    let consumer_secret = console_input("Paste your consumer secret");
+
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+
+    let con_token = egg_mode::KeyPair::new(consumer_key.clone(), consumer_secret.clone());
+    let request_token = core.run(egg_mode::request_token(&con_token, "oob", &handle))
+        .unwrap();
+    println!(
+        "Click this link to authorize on Twitter: {}",
+        egg_mode::authorize_url(&request_token)
+    );
+    let pin = console_input("Paste your PIN");
+
+    let (token, user_id, screen_name) = core.run(egg_mode::access_token(
+        con_token,
+        &request_token,
+        pin,
+        &handle,
+    )).unwrap();
+
+    match token {
+        egg_mode::Token::Access {
+            access: ref access_token,
+            ..
+        } => {
+            let twitter_config = TwitterConfig {
+                consumer_key: consumer_key,
+                consumer_secret: consumer_secret,
+                access_token: access_token.key.to_string(),
+                access_token_secret: access_token.secret.to_string(),
+                user_id: user_id,
+                user_name: screen_name,
+            };
+            // Save app data for using on the next run.
+            let toml = toml::to_string(&twitter_config).unwrap();
+            let mut file = File::create("twitter.toml").unwrap();
+            file.write_all(toml.as_bytes()).unwrap();
+
+            return twitter_config;
+        }
+        _ => unreachable!(),
+    }
+}
+
+fn twitter_load_from_config(mut file: File) -> TwitterConfig {
+    let mut config = String::new();
+    file.read_to_string(&mut config).unwrap();
+    toml::from_str(&config).unwrap()
+}
+
+fn console_input(prompt: &str) -> String {
+    println!("{}: ", prompt);
+    let mut line = String::new();
+    let _ = io::stdin().read_line(&mut line).unwrap();
+    line.trim().to_string()
 }
