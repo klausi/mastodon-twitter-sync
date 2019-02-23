2fa5215429b2273b858a08412fbafe9de6d70d15
Klaus Purer <klaus.purer@protonmail.ch>
2019-02-23 22:11:37 +0100

    refactor(serde): Use derive as feature of serde, bump version

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use chrono::prelude::*;
 use mammut::Data;
-use serde_derive::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::fs::remove_file;
 use std::fs::File;
