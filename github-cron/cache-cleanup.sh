747840857372603e8a19b300f27f709d5d0f915f
Klaus Purer <klaus.purer@protonmail.ch>
2023-01-04 13:28:23 +0100

    remove hard-coded repo name

--- github-cron/cache-cleanup.sh
+++ github-cron/cache-cleanup.sh
@@ -6,7 +6,7 @@
 RESPONSE=$(curl -i \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN"  \
-  https://api.github.com/repos/klausi/mts-cron/actions/caches )
+  "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/actions/caches" )
 echo "$RESPONSE"
 CACHE_IDS=( $(echo "$RESPONSE" | grep '"id"' | grep -o '[0-9]*') )
 
@@ -17,5 +17,5 @@ do
   -X DELETE \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
-  "https://api.github.com/repos/klausi/mts-cron/actions/caches/$CACHE_ID"
+  "$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/actions/caches/$CACHE_ID"
 done
